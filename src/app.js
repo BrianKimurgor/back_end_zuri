f4cd218f35982cd693ad41009e1054b0db082a22
Brian Kimurgor <kimurgorbrian20@gmail.com>
2023-09-11 16:01:23 +0300

    finalize

--- src/app.js
+++ src/app.js
@@ -1,29 +1,32 @@
 const express = require('express');
-   const app = express();
+const app = express();
 
-   app.get('/api', (req, res) => {
-     const slackName = req.query.slack_name;
-     const track = req.query.track;
-     const currentDay = new Date().toLocaleDateString('en-US', { weekday: 'long' });
-     const utcTime = new Date().toISOString();
-     const githubFileUrl = 'https://github.com/BrianKimurgor/back_end_zuri/blob/main/src/app.js';
-     const githubRepoUrl = 'https://github.com/BrianKimurgor/back_end_zuri';
-     const statusCode = 200;
+app.get('/api', (req, res) => {
+  const slackName = req.query.slackName;
+  const track = req.query.track;
+  const currentDay = new Date().toLocaleDateString('en-US', { weekday: 'long' });
+  const utcTime = new Date().toISOString();
+  const githubFileUrl = 'https://github.com/BrianKimurgor/back_end_zuri/blob/main/src/app.js';
+  const githubRepoUrl = 'https://github.com/BrianKimurgor/back_end_zuri';
+  const statusCode = 200;
 
-     const responseData = {
-       slack_name: "Bran Kimurgor",
-       current_day: currentDay,
-       utc_time: utcTime,
-       track: 'backend',
-       github_file_url: githubFileUrl,
-       github_repo_url: githubRepoUrl,
-       status_code: statusCode
-     };
+  const responseData = {
+    slack_name: 'Brian Kimurgor',
+    current_day: currentDay,
+    utc_time: utcTime,
+    track: 'backend',
+    github_file_url: githubFileUrl,
+    github_repo_url: githubRepoUrl,
+    status_code: statusCode
+  };
 
-     res.json(responseData);
-   });
+  const formattedOutput = JSON.stringify(responseData, null, 2);
 
-   const port = process.env.PORT || 3000;
-   app.listen(port, () => {
-     console.log(`Server is running on port ${port}`);
-   });
+  res.setHeader('Content-Type', 'application/json');
+  res.send(formattedOutput);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
